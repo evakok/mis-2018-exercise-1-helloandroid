45538b671b2546762a616416fdb17dfcc0572ddd
Hiyeon <ohyeahbravo@gmail.com>
2018-04-12 17:44:23 +0200

    afternoon trial

--- app/src/main/java/com/example/mis/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/mis/helloandroid/MainActivity.java
@@ -1,131 +1,106 @@
 package com.example.mis.helloandroid;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.text.TextUtils;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.charset.Charset;
 
 
 public class MainActivity extends AppCompatActivity {
-    private EditText uriText;
-    private Button connect;
-    private ImageView imageresponse;
-    public TextView response;
+
+    TextView txtWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        connect = (Button) findViewById(R.id.button);
-        uriText = (EditText) findViewById(R.id.url_input);
-        TextView response = (TextView) findViewById(R.id.response);
-        imageresponse = (ImageView) findViewById(R.id.image_response);
-        /*
-        connect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View View) {
-                getResponseFromHttpUrl();
-            }
-        }); */
     }
 
-    public static String getResponseFromHttpUrl() throws IOException {
+    public void HTTPConnection(View view) throws IOException {
+
+        String output = "";
+        URL url = getURL();
+
+        output = readFromStream(makeHttpRequest(url));
+        showTxt(output);
+    }
+
+    public URL getURL() {
+
         URL url = null;
 
-        try {
-            url = new URL("http://www.android.com");
+        String url_string = ((EditText)findViewById(R.id.url_entry)).getText().toString();
+
+        try{
+            url = new URL(url_string);
         } catch (MalformedURLException e) {
-            e.printStackTrace();
+            Toast malUrlErr = Toast.makeText(MainActivity.this, "Malformed URL", Toast.LENGTH_SHORT);
+            malUrlErr.setGravity(Gravity.CENTER, 0 , 0);
+            malUrlErr.show();
+            return null;
         }
 
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        return url;
+    }
+
+    public void showTxt(String txt) {
+        txtWindow = (TextView) findViewById(R.id.response_view);
+        txtWindow.setText(txt);
+    }
+
+    public void showMsg(String txt){
+        Toast toast = Toast.makeText(MainActivity.this, txt, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    public HttpURLConnection makeHttpRequest(URL url) throws IOException {
+        if (url == null)
+            return null;
+        HttpURLConnection urlConnection = null;
         try {
-            InputStream in = urlConnection.getInputStream();
-
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+            if(urlConnection.getResponseCode() != 200){
+                throw IOException;
             }
-
-            
-
+        } catch(IOException) {
+            showMsg("went wrong");
         } finally {
-            urlConnection.disconnect();
-        }
-    }
-
-        /*//Connect button is pressed
-        public void ConnectToURL(EditText urltext){
-
-            try {
-                URL finalurl = new URL("http://www.android.com");
-                HttpURLConnection urlConnection = (HttpURLConnection) finalurl.openConnection();
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                readStream(in);
+            if(urlConnection != null) {
                 urlConnection.disconnect();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-
-
-        }*/
-
-//        connect.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View View){
-//                openBrowser();
-//            }
-//            });
-//        uriText.setOnKeyListener(new View.OnKeyListener() {
-//            public boolean onKey(View view, int keyCode, KeyEvent event) {
-//                if (keyCode == KeyEvent.KEYCODE_ENTER) {
-//                    openBrowser();
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//        }
-//        private void openBrowser() {
-//            String uri = uriText.getText().toString();
-//            if(uri.indexOf("http:/www.") < 0 ){
-//                uri = "http://www." + uri;
-//            }
-//            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-//            startActivity(intent);
-//        }
-        //public void HTTPConnection(View view) {
-        //URL url = new URL();
-        //Toast toast = Toast.makeText(MainActivity.this, "hey there", Toast.LENGTH_SHORT);
-        //toast.setGravity(Gravity.CENTER, 0 ,0);
-        //toast.show();
-        //}
+        }
+        return urlConnection;
     }
 
+    public String readFromStream(HttpURLConnection urlConnection) throws IOException{
+        InputStream inputStream = urlConnection.getInputStream();
+        StringBuilder output = new StringBuilder();
+        if(inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while(line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+}
